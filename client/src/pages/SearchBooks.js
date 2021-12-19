3edd5095a79ac31712d65b81fb98450608e2c048
Ian Greenblott <imgreenblott@gmail.com>
2021-12-19 13:11:37 -0500

    client, ediditn copy. Trying to figure out plan of attack,here.

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -88,7 +88,7 @@ const SearchBooks = () => {
     <>
       <Jumbotron fluid className='text-light bg-dark'>
         <Container>
-          <h1>Search for Books!</h1>
+          <h1>Search by Topic</h1>
           <Form onSubmit={handleFormSubmit}>
             <Form.Row>
               <Col xs={12} md={8}>
@@ -98,12 +98,12 @@ const SearchBooks = () => {
                   onChange={(e) => setSearchInput(e.target.value)}
                   type='text'
                   size='lg'
-                  placeholder='Search for a book'
+                  placeholder='Search by proverb'
                 />
               </Col>
               <Col xs={12} md={4}>
                 <Button type='submit' variant='success' size='lg'>
-                  Submit Search
+                  Submit
                 </Button>
               </Col>
             </Form.Row>
@@ -115,7 +115,7 @@ const SearchBooks = () => {
         <h2>
           {searchedBooks.length
             ? `Viewing ${searchedBooks.length} results:`
-            : 'Search for a book to begin'}
+            : 'Search for a topic to begin'}
         </h2>
         <CardColumns>
           {searchedBooks.map((book) => {
